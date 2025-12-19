718a37cf3666e257813224da302992f1e97144cd
Mcl0103 <marlonfa123@gmail.com>
2025-12-18 22:04:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-import uuid
 import subprocess
+import uuid
 import os
 
 app = FastAPI()
 
-# Permitir llamadas desde cualquier web
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -26,31 +25,10 @@ def root():
 
 @app.post("/descargar")
 def descargar_video(data: Link):
-    video_id = str(uuid.uuid4())
-    output_file = f"{video_id}.mp4"
+    return JSONResponse(
+        content={
+            "status": "ok",
+            "url_recibida": data.url
+        }
+    )
 
-    try:
-      subprocess.run(
-    [
-        "yt-dlp",
-        data.url,
-        "-o",
-        output_file,
-        "--recode-video",
-        "mp4",
-        "--postprocessor-args",
-        "-c:v libx264 -pix_fmt yuv420p",
-    ],
-    check=True,
-)
-
-
-
-        return FileResponse(
-            path=output_file,
-            filename="video.mp4",
-            media_type="video/mp4",
-        )
-
-    except Exception as e:
-        return {"error": str(e)}
