46491e566de47a446f4d3259dec760a730f1a94f
Mcl0103 <marlonfa123@gmail.com>
2025-12-18 21:33:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Link(BaseModel):
+    url: str
 
 @app.get("/")
 def root():
     return {"status": "backend vivo"}
+
+@app.post("/recibir")
+def recibir_link(link: Link):
+    return {
+        "message": f"Backend recibió el link: {link.url}"
+    }
